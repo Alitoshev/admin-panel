1c3a65e3577617b1eca65dac500f655e508df7e4
Diyorbek <alitoshevdiyorbek@gmail.com>
2023-08-17 15:16:07 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { DarkModeContextProvider } from "./context/darkModeContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <DarkModeContextProvider>
+    <App />
+  </DarkModeContextProvider>
+);
---
+++ src/pages/home/components/navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import { useContext } from "react";
+import { DarkModeContext } from "../../../../context/darkModeContext";
+import "./navbar.scss";
+import {
+  ChatBubbleOutlineOutlined,
+  DarkModeOutlined,
+  FullscreenExitOutlined,
+  LanguageOutlined,
+  ListOutlined,
+  NotificationsNoneOutlined,
+  Search,
+} from "@mui/icons-material";
+
+function Navbar() {
+  const { dispatch } = useContext(DarkModeContext);
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <Search />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined className="icon" />
+            Engling
+          </div>
+          <div className="item">
+            <DarkModeOutlined
+              className="icon"
+              onClick={() => dispatch({ type: "TOGGLE" })}
+            />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlined className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+              alt=""
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
